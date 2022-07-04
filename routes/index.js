e731c02e0900fad7352b8a67bb7aea1be243c5b3
Michael Peters <michaelrespond99@gmail.com>
2022-07-04 11:52:11 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,11 @@ import { guardartestimonial } from '../controllers/testimonialcontroller.js';
 
 const router = express.Router();
 
-router.get('/', paginaInicio);
-router.get('/nosotros', paginaNosotros);
-router.get('/testimoniales', paginaTestimoniales);
-router.post('/testimoniales', guardartestimonial);
-router.get('/viajes', paginaViajes);
-router.get('/viajes/:slug', paginaDetalleViaje);
+router.get('./', paginaInicio);
+router.get('./nosotros', paginaNosotros);
+router.get('./testimoniales', paginaTestimoniales);
+router.post('./testimoniales', guardartestimonial);
+router.get('./viajes', paginaViajes);
+router.get('./viajes/:slug', paginaDetalleViaje);
 
 export default router;
